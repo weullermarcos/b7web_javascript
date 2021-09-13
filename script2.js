f08c9ce8f4c62fd89c804ca08fd3f7aec2be6d6f
Weuller <weullermarcos@hotmail.com>
2021-09-13 17:13:04 -0300

    Requisição para inclusão de posts

--- script2.js
+++ script2.js
@@ -1,4 +1,25 @@
 
+
+async function inserirPosts(){
+
+    document.getElementById('posts').innerHTML = 'Carregando...';
+
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'titulo de teste',
+            body: 'corpo de teste',
+            userId: 4
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    let json = await req.json();
+
+}
+
 async function carregarPosts(){
 
     let req = await fetch('https://jsonplaceholder.typicode.com/posts');
