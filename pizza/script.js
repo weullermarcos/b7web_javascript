7cbbb0babc06a1dd4710cc0732b757c80dd4c892
Weuller <weullermarcos@hotmail.com>
2021-10-06 11:00:14 -0300

    ação de incrementar e decrementar quantidades

--- pizza/script.js
+++ pizza/script.js
@@ -66,8 +66,31 @@ function closeModal(){
     setTimeout(()=>{
         c('.pizzaWindowArea').style.display = 'none';
     }, 500);
-
-
-
 }
+
+//ação de incrementar quantidade
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+
+    if(modalQt > 1){
+
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+
+//ação de decrementar quantidade
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+//ação de selecionar o tamanho
+cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
 
